d8d5bdd91c18056031d9a5d43453dc330d2f6a23
Matthew Dolan <mdolancode@gmail.com>
2023-01-30 10:21:14 -0800

    Getting started with Combine. Blog Post.

--- Combine+UIKit/ViewController.swift
+++ Combine+UIKit/ViewController.swift
@@ -6,14 +6,44 @@
 //
 
 import UIKit
+import Combine
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-
+// Setup notification centre based publisher
+extension Notification.Name {
+    static let newBlogPost = Notification.Name("newPost")
 }
 
+struct BlogPost {
+    let title: String
+}
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var blogTextField: UITextField!
+    @IBOutlet var publishedButton: UIButton!
+    @IBOutlet var subscribedLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        publishedButton.addTarget(self, action: #selector(publishButtonTapped), for: .primaryActionTriggered)
+        
+        
+        // Create a publisher
+        let publisher = NotificationCenter.Publisher(center: .default, name: .newBlogPost, object: nil)
+            .map { (notification) -> String? in
+                return (notification.object as? BlogPost)?.title ?? ""
+            }
+        
+        // Create a subscriber
+        let subscriber = Subscribers.Assign(object: subscribedLabel, keyPath: \.text)
+        publisher.subscribe(subscriber)
+    }
+    
+    @objc func publishButtonTapped(_ sender: UIButton) {
+        // Post the notification
+        let title = blogTextField.text ?? "Coming soon"
+        let blogPost = BlogPost(title: title)
+        NotificationCenter.default.post(name: .newBlogPost, object: blogPost)
+    }
+}
